98d523db7edee0d2ce2f575ec89566c72dbd7721
lc-dmx <15505164810@163.com>
2018-12-11 09:29:35 +1100

    Update Solution.java

--- 006 ZigZag Conversion/Solution.java
+++ 006 ZigZag Conversion/Solution.java
@@ -1,1 +1,20 @@
+if(numRows == 1) return s;
+        String result = "";
+        int len = 2 * (numRows - 1);
+        
+        for(int i = 0; i < numRows; i++){
+        	int j = 0;
+            for( ; j + i < s.length(); ){
 
+                result = result + s.charAt(j + i);
+                j = j + len;
+                
+                if(i != 0 && i != numRows - 1 && j - i < s.length()){
+                	
+                    result = result + s.charAt(j - i);
+                    
+                }
+            
+            }
+        }
+        return result;
