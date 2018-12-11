568d43bcac6a42479137b5cc75086888c841a282
lc-dmx <15505164810@163.com>
2018-12-11 21:02:11 +1100

    Update Solution.java

--- 006 ZigZag Conversion/Solution.java
+++ 006 ZigZag Conversion/Solution.java
@@ -1,20 +1,21 @@
-if(numRows == 1) return s;
-        String result = "";
-        int len = 2 * (numRows - 1);
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
         
-        for(int i = 0; i < numRows; i++){
-        	int j = 0;
-            for( ; j + i < s.length(); ){
-
-                result = result + s.charAt(j + i);
-                j = j + len;
-                
-                if(i != 0 && i != numRows - 1 && j - i < s.length()){
-                	
-                    result = result + s.charAt(j - i);
-                    
+        int len = s.length();
+        int span = 2 * (numRows - 1);
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < numRows; i++) {
+            int j = 0;
+            for (; i + j < len; ) {
+                result.append(s.charAt(i + j));
+                j = j + span;
+                if (i != 0 && i != numRows - 1 && j - i < len) {
+                    result.append(s.charAt(j - i)); 
                 }
-            
             }
         }
-        return result;
+        return result.toString();
+    }
+}
