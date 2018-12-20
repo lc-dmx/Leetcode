a5b6b1368b1c25a7fa85220c31f08f565a2643de
lc-dmx <15505164810@163.com>
2018-12-20 15:40:16 +1100

    Update Solution.java

--- 014 Longest Common Prefix/Solution.java
+++ 014 Longest Common Prefix/Solution.java
@@ -1,24 +1,38 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {        
-        String result = "";
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";    
+        }
         
-        if(strs == null || strs.length == 0){
-        	return result;
+        String result = "";
+
+        int len = strs.length;
+        int minWordLen = strs[0].length();
+        int minWordPos = 0;
+        for (int i = 1; i < len; i++) {
+            if (strs[i].length() < minWordLen) {
+                minWordLen = strs[i].length();
+                minWordPos = i;
+            }
         }
 
-        for(int i = 0; i < strs[0].length(); i++){
-        	int j = 1;
-        	while(j < strs.length){
-        		if(i == strs[j].length() || strs[0].charAt(i) != strs[j].charAt(i)){
-        			return result;
-            	}else{
-            		j++;
-            	}
-        	}
-        	if(j == strs.length){
-        		result = result + strs[0].charAt(i);
-        	}
+        int j = 0;
+        while (j < minWordLen) {
+            int i = 0;
+            while (i < len) {
+                if (strs[minWordPos].charAt(j) == strs[i].charAt(j)) {
+                    i++;
+                } else {
+                    return result;
+                }
+            }
+            if (i == len) {
+                j++;
+                result = strs[minWordPos].substring(0, j);
+            }
         }
+
+
         return result;
     }
 }
