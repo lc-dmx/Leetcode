8d245260b9a2bb425e25031fa288f4d6f6c6d4dd
lc-dmx <15505164810@163.com>
2019-01-14 20:55:03 +1100

    Update Solution.java

--- 026 Remove Duplicates from Sorted Array/Solution.java
+++ 026 Remove Duplicates from Sorted Array/Solution.java
@@ -1,3 +1,12 @@
+Approach 1: Two Pointers
+Algorithm
+
+Since the array is already sorted, we can keep two pointers i and j, where i is the slow-runner while j is the fast-runner. 
+As long as nums[i] = nums[j], we increment j to skip the duplicate.
+
+When we encounter nums[j] doesn't equal to nums[i], the duplicate run has ended so we must copy its value to nums[i + 1]. 
+i is then incremented and we repeat the same process again until j reaches the end of array.
+	
 class Solution {
     public int removeDuplicates(int[] nums) {
         if(nums.length == 0) return 0;
