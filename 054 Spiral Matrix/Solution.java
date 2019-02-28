857852fb21a13eccfb0f35fc26b92ca8a513c825
lc-dmx <15505164810@163.com>
2019-02-28 12:07:06 +1100

    Update Solution.java

--- 054 Spiral Matrix/Solution.java
+++ 054 Spiral Matrix/Solution.java
@@ -44,4 +44,46 @@ Complexity Analysis
 
 Solution 2: Layer-by-layer
 
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix.length == 0) {
+            return result;
+        }
 
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; c++) {
+                result.add(matrix[r1][c]);
+            }
+
+            for (int r = r1 + 1; r <= r2; r++) {
+                result.add(matrix[r][c2]);
+            }
+
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; c--) {
+                    result.add(matrix[r2][c]);
+                }
+
+                for (int r = r2; r > r1; r--) {
+                    result.add(matrix[r][c1]);
+                }
+            }
+
+            r1++;
+            c1++;
+            r2--;
+            c2--;
+        }
+        return result;
+    }
+}
+
+Complexity Analysis
+
+* Time Complexity: O(N), where N is the total number of elements in the input matrix. We add every element in the matrix to our final answer.
+
+* Space Complexity: O(N), the information stored in ans.
