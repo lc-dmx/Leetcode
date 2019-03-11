d83d402560e82102dbe95a9d59f7f830ddc6070e
lc-dmx <15505164810@163.com>
2019-03-11 17:40:16 +1100

    Update Solution.java

--- 046 Permutations/Solution.java
+++ 046 Permutations/Solution.java
@@ -1,34 +1,20 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return result;
-        }
-        int start = 0;
-        permutation(result, nums, start);
-        return result;
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
     }
 
-    private void permutation(List<List<Integer>> result, int nums[], int start) {
-        if (start == nums.length - 1) {
-            ArrayList<Integer> array = new ArrayList<>();
-            for (int i= 0; i < nums.length; i++) {
-                array.add(nums[i]);
-            }
-            result.add(array);
-        }
-        for (int i = start; i < nums.length; i++) {
-            if (i == start || nums[i] != nums[start]) {
-                swap(nums, i, start);
-                permutation(result, nums, start + 1);
-                swap(nums, i, start);
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+        if (tempList.size() == nums.length) {
+            list.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (tempList.contains(nums[i])) continue; // element already exists, skip
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
             }
         }
-    }
-    
-    private void swap(int[] nums, int index1, int index2) {
-        int temp = nums[index1];
-        nums[index1] = nums[index2];
-        nums[index2] = temp;
     }
 }
