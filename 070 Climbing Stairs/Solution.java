14b1891ce351449a0065c28847d5c41a00d982c9
lc-dmx <15505164810@163.com>
2019-03-26 20:52:04 +1100

    Create Solution.java

---
+++ 070 Climbing Stairs/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] results = new int[n];
+        results[0] = 1;
+        results[1] = 2;
+        for (int i = 2; i < n; i++) {
+            results[i] = results[i - 1] + results[i - 2];
+        }
+        return results[n - 1];
+    }
+}
