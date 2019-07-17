919d37d7560ccbb30a8b50422482df5e4d72ae3b
lc-dmx <15505164810@163.com>
2019-07-17 12:55:08 +1000

    Create Solution.java

---
+++ 001-100/095 Unique Binary Search Trees II/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return dfs(1, n);
+    }
+
+    private List<TreeNode> dfs(int start, int end) {
+        List<TreeNode> result = new ArrayList<>();
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftNodes = dfs(start, i - 1);
+            List<TreeNode> rightNodes = dfs(i + 1, end);
+
+            for (TreeNode left : leftNodes) {
+                for (TreeNode right : rightNodes) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    result.add(root);
+                }
+            }
+        }
+
+        return result;
+    }
+}
