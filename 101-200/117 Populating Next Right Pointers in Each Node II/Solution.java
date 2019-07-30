28492f7b41b2ee22c7a74220c7ac7004d1b1864b
lc-dmx <15505164810@163.com>
2019-07-30 13:06:04 +1000

    Update Solution.java

--- 101-200/117 Populating Next Right Pointers in Each Node II/Solution.java
+++ 101-200/117 Populating Next Right Pointers in Each Node II/Solution.java
@@ -16,6 +16,11 @@ class Node {
     }
 };
 */
+
+这道题跟I的区别就是binary tree不是完全二叉树。
+所以root.right.next就不一定等于root.next.left。
+所以，目标就是先确定好root的右孩子的第一个有效next连接点，然后再处理左孩子。
+
 class Solution {
     public Node connect(Node root) {
         if (root != null) {
