90747a56a63a7f44e024c77f7c9a55e936306cec
lc-dmx <15505164810@163.com>
2019-07-30 14:38:04 +1000

    Update Solution.java

--- 101-200/119 Pascal's Triangle II/Solution.java
+++ 101-200/119 Pascal's Triangle II/Solution.java
@@ -1,13 +1,10 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> result = new ArrayList<>(rowIndex + 1);
-        for (int i = 0; i <= rowIndex; i++) {
-            result.add(0);
-        }
-        result.set(0, 1);
-        
+        result.add(1);
+
         for (int i = 1; i <= rowIndex; i++) {
-            
+            result.add(0);
             for (int j = i; j >= 1; j--) {
                 result.set(j, result.get(j) + result.get(j - 1));
             }
