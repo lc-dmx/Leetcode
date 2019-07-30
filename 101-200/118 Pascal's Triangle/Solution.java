892e1895f0c22442217167e53b25b2c66a08d45f
lc-dmx <15505164810@163.com>
2019-07-30 13:47:02 +1000

    Create Solution.java

---
+++ 101-200/118 Pascal's Triangle/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) {
+            return result;
+        }
+
+        result.add(new ArrayList<>(Collections.singletonList(1)));
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> temp = new ArrayList<>();
+
+            temp.add(1);
+            for (int j = 1; j < i; j++) {
+                temp.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+            }
+            temp.add(1);
+            result.add(temp);
+        }
+        return result;
+    }
+}
