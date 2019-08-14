60eadab67fcc9f6bd467e877198c349a5c0c4e66
lc-dmx <15505164810@163.com>
2019-08-14 13:24:43 +1000

    Create Solution.java

---
+++ 200-300/207 Course Schedule/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    boolean flag = true;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        //初始化所有节点均未被访问过
+        int[] visit = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            visit[i] = -1;
+        }
+
+        //建立邻接表
+        for (int i = 0; i < prerequisites.length; i++) {
+            adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            dfs(adjList, i, visit);
+            if (!flag) {
+                return false;
+            }
+        }
+        return flag;
+    }
+
+    private void dfs(List<List<Integer>> adjList, int node, int[] visit) {
+        visit[node] = 0;
+
+        for (int i = 0; i < adjList.get(node).size(); i++) {
+            if (visit[adjList.get(node).get(i)] == -1) {
+                dfs(adjList, adjList.get(node).get(i), visit);
+            } else if (visit[adjList.get(node).get(i)] == 0) {
+                flag = false;
+            }
+        }
+
+        visit[node] = 1;
+    }
+}
