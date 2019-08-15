63e82156c9fc42e774c5db2a27507bf4e5eeab0d
lc-dmx <15505164810@163.com>
2019-08-15 11:27:25 +1000

    Update Solution.java

--- 200-300/210 Course Schedule II/Solution.java
+++ 200-300/210 Course Schedule II/Solution.java
@@ -1,3 +1,5 @@
+dfs, 逆序输出
+# Solution 1
 class Solution {
     boolean flag = true;
     Stack<Integer> stack = new Stack<>();
@@ -48,5 +50,54 @@ class Solution {
 
         visit[node] = 1;
         stack.push(node);
+    }
+}
+
+根据入度判断
+# Solution 2
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[numCourses];
+        int[] topologicalOrder = new int[numCourses];
+
+        //建立邻接表
+        for (int i = 0; i < prerequisites.length; i++) {
+            adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            indegree[prerequisites[i][0]] += 1;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        //添加入度为0的点
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int i = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            topologicalOrder[i++] = node;
+
+            //把该点的所有邻居的入度减1
+            for (Integer neighbor : adjList.get(node)) {
+                indegree[neighbor]--;
+
+                if (indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        if (i == numCourses) {
+            return topologicalOrder;
+        }
+
+        return new int[0];
     }
 }
