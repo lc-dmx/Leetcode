1e881d64e53f210c0ef670c730a1c092040cabfe
lc-dmx <15505164810@163.com>
2019-08-15 11:32:35 +1000

    Update Solution.java

--- 200-300/207 Course Schedule/Solution.java
+++ 200-300/207 Course Schedule/Solution.java
@@ -1,3 +1,5 @@
+dfs
+# Solution 1
 class Solution {
     boolean flag = true;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
@@ -38,5 +40,53 @@ class Solution {
         }
 
         visit[node] = 1;
+    }
+}
+
+入度
+# Solution 2
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[numCourses];
+
+        //建立邻接表
+        for (int i = 0; i < prerequisites.length; i++) {
+            adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            indegree[prerequisites[i][0]] += 1;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        //添加入度为0的点
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int i = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            i++;
+
+            //把该点的所有邻居的入度减1
+            for (Integer neighbor : adjList.get(node)) {
+                indegree[neighbor]--;
+
+                if (indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        if (i == numCourses) {
+            return true;
+        }
+
+        return false;
     }
 }
