580faf861c0e0d28f30b6407db260bd91cc64571
lc-dmx <15505164810@163.com>
2019-10-20 15:28:56 +1100

    Create Solution.java

---
+++ 201-300/229 Majority Element II/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        int num1 = -1;
+        int count1 = 0;
+        int num2 = -1;
+        int count2 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num1) {
+                count1++;
+            } else if (nums[i] == num2) {
+                count2++;
+            } else if (count1 == 0) {
+                num1 = nums[i];
+                count1 = 1;
+            } else if (count2 == 0) {
+                num2 = nums[i];
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num1) {
+                count1++;
+            } else if (nums[i] == num2) {
+                count2++;
+            }
+        }
+
+        if (count1 > nums.length / 3) result.add(num1);
+        if (count2 > nums.length / 3) result.add(num2);
+        return result;
+    }
+}
